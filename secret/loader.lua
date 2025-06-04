1982fc3b220b5c167f267c5c8448ee5fc4ffe85d
CrashCover123 <usermine78yt@yandex.ru>
2025-06-04 17:37:34 +0300

    Update loader.lua

--- secret/loader.lua
+++ secret/loader.lua
@@ -1,35 +1,29 @@
-repeat wait() until game:IsLoaded()
+repeat task.wait() until game:IsLoaded()
 
-if Key == nil then
+if getgenv().Key == nil then
     getgenv().Key = "ZeroFlux HUB"
 end
 
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 
---function sendnotification(title, subtitle)
-	--Fluent:Notify({
-		--Title = "ZeroFlux HUB",
-		--Content = message,
-		--SubContent = submessage, -- Optional
-		--Duration = 5 -- Set to nil to make the notification not disappear
-	--})
---end
 local player = game.Players.LocalPlayer
+
 function sendnotification(message, type, submessage)
     if type == false or type == nil then
-        print("[ " .. Key .. " ]: " .. message)
+        print("[ " .. getgenv().Key .. " ]: " .. message)
     end
     if type == true or type == nil then
         Fluent:Notify({
-			Title = Key,
-			Content = message,
-			SubContent = submessage, -- Optional
-			Duration = 5 -- Set to nil to make the notification not disappear
-		})
+            Title = getgenv().Key,
+            Content = message,
+            SubContent = submessage, -- Optional
+            Duration = 5 -- Set to nil to make the notification not disappear
+        })
     end
-en
+end
 
 sendnotification("Loader executed.", false)
+
 --------------------------------------------------------------------------------------DEFINE----------------------------------------------------------------------------------------
 local UIS = game:GetService("UserInputService")
 local Touchscreen = UIS.TouchEnabled
@@ -42,34 +36,31 @@ local games = {
 
 --------------------------------------------------------------------------------------WHITE LIST-------------------------------------------------------------------------------
 local listplayer = {
-	["YaNeSkazy"] = true,
-	["OTBEPTKA_2006"] = true,
-	["KotKotoveevich"] = true,
-	["lsad775"] = true,
+    ["YaNeSkazy"] = true,
+    ["OTBEPTKA_2006"] = true,
+    ["KotKotoveevich"] = true,
+    ["lsad775"] = true,
 }
 
 local function checkPlayer(player)
-	if listplayer[player.Name] then
-		print(player.Name .. " успешно прошёл проверку белого списка.")
-		sendnotification("Вы успешно прошли проверку белого списка.", nil)
-		return true
-	else
-		sendnotification("Доступ запрещён: вы не в белом списке!", nil)
-		wait(1)
-		player:Kick("[ZeroFlux]/n Доступ запрещён: вы не в белом списке!")
-		return false
-	end
+    if listplayer[player.Name] then
+        print(player.Name .. " успешно прошёл проверку белого списка.")
+        sendnotification("Вы успешно прошли проверку белого списка.", nil)
+        return true
+    else
+        sendnotification("Доступ запрещён: вы не в белом списке!", nil)
+        task.wait(1)
+        player:Kick("[ZeroFlux]\nДоступ запрещён: вы не в белом списке!")
+        return false
+    end
 end
+
 --------------------------------------------------------------------------------------LINK VERIFICATION-------------------------------------------------------------------------------
 local scriptUrl = 'https://raw.githubusercontent.com/CrashCover123/ZeroFluxHUB/refs/heads/main/secret/loader.lua'
 
-
 if not string.find(scriptUrl, 'CrashCover123') or scriptUrl ~= 'https://raw.githubusercontent.com/CrashCover123/ZeroFluxHUB/refs/heads/main/secret/loader.lua' then
     sendnotification("Ты используешь ПОДДЕЛЬНЫЙ скрипт  ❌")
-	while true
-		error("Detected fake script")
-	end
-	player:Kick("[ZeroFlux]/n Ты используешь ПОДДЕЛЬНЫЙ скрипт ❌")
+    player:Kick("[ZeroFlux]\nТы используешь ПОДДЕЛЬНЫЙ скрипт ❌")
     return
 else
     sendnotification("Подтвержденная ссылка и создатель ✅")
@@ -81,21 +72,20 @@ sendnotification(R3TH_Device .. " detected.", false)
 
 if getgenv().R3TH_Device == "Mobile" then
     sendnotification("Мобильные устройства не поддерживаются, только ПК", nil)
-    wait(2)
-    player:Kick("[ZeroFlux]/n Мобильные устройства не поддерживаются, только ПК")
+    task.wait(2)
+    player:Kick("[ZeroFlux]\nМобильные устройства не поддерживаются, только ПК")
     return
 end
 
 sendnotification("Загрузка скрипта может занять некоторое время в зависимости от вашего ПК.", nil)
-wait(2)
+task.wait(2)
 
 --------------------------------------------------------------------------------------LOADER----------------------------------------------------------------------------------------
 if games[game.PlaceId] then
-    sendnotification("Игра подерживается ✅", nil)
+    sendnotification("Игра поддерживается ✅", nil)
     loadstring(game:HttpGet('https://raw.githubusercontent.com/CrashCover123/ZeroFluxHUB/refs/heads/main/game/games/' .. games[game.PlaceId] .. '.lua'))()
 else
-    sendnotification("Игра не подерживается ❌", nil)
-	wait(2)
-	player:Kick("[ZeroFlux] Игра не подерживается ❌")
+    sendnotification("Игра не поддерживается ❌", nil)
+    task.wait(2)
+    player:Kick("[ZeroFlux]\nИгра не поддерживается ❌")
 end
-
